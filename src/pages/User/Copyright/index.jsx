3bdb66b7349bac1defddc4b8d8cb1e712d24f9e6
nicorey89 <nicolasthian8907@gmail.com>
2023-08-01 21:23:49 -0300

    abdhabd

--- src/pages/User/Copyright/index.jsx
+++ src/pages/User/Copyright/index.jsx
@@ -5,7 +5,7 @@ export default function Copyright(props) {
     return (
       <Typography variant="body2" color="white" align="center" {...props}>
         {'Copyright © '}
-        <Link color="inherit" to="https://nicoreydev.vercel.app">
+        <Link color="inherit" to="https://nicolas-rey.vercel.app/">
           Nico Rey
         </Link>{' '}
         {new Date().getFullYear()}
